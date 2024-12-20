308c33a9abda9a6afd80c7ea7df5a72d1ad01e43
raodenn <dtaye144@gmail.com>
2024-12-20 17:07:45 +0300

    fixed a few bugs and added colors

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ var gameEnded = false;
 canvas.width = 500;
 canvas.height = 500;
 
-var directionX = 3;
+var directionX = 10;
 var directionY = 0;
 var snakeSegments = [];
 var snakeLength = 1;
-var snakeX = 0;
-var snakeY = 0;
-
+var snakeX = 250;
+var snakeY = 250;
 var dots = [];
+
+function drawBackground() {
+    canvas2d.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+}
+
+function drawApple(x, y) {
+    canvas2d.fillStyle = "red";
+    canvas2d.beginPath();
+    canvas2d.arc(x + 5, y + 5, 5, 0, Math.PI * 2);
+    canvas2d.fill();
+
+    canvas2d.fillStyle = "green";
+    canvas2d.fillRect(x + 4, y - 2, 3, 4); 
+}
 
 function spawnDots() {
     if(dots.length < 10) {
@@ -21,31 +34,38 @@ function spawnDots() {
       dots.push({ x: dotX, y: dotY });
     }
     for (var i = 0; i < dots.length; i++) {
-        canvas2d.fillStyle = "red";
-        canvas2d.fillRect(dots[i].x, dots[i].y, 10, 10);
-      }
+          
+        drawApple(dots[i].x, dots[i].y);
+    }
   }
   
-
-document.onkeydown = function(event) {
+  document.onkeydown = function (event) {
     switch (event.code) {
-        case "ArrowLeft": // Left arrow
-          directionX = -10;
-          directionY = 0;
-          break;
-        case "ArrowUp": // Up arrow
-          directionX = 0;
-          directionY = -10;
-          break;
-        case "ArrowRight": // Right arrow
-          directionX = 10;
-          directionY = 0;
-          break;
-        case "ArrowDown": // Down arrow
-          directionX = 0;
-          directionY = 10;
-          break;
-      }
+        case "ArrowLeft": 
+            if (directionX === 0) { 
+                directionX = -10;
+                directionY = 0;
+            }
+            break;
+        case "ArrowUp": 
+            if (directionY === 0) {
+                directionX = 0;
+                directionY = -10;
+            }
+            break;
+        case "ArrowRight":
+            if (directionX === 0) { 
+                directionX = 10;
+                directionY = 0;
+            }
+            break;
+        case "ArrowDown": 
+            if (directionY === 0) { 
+                directionX = 0;
+                directionY = 10;
+            }
+            break;
+    }
 };
 
 function moveSnake() {
@@ -79,15 +99,31 @@ function checkCollision() {
           }
       }
   }
-  
-
+ 
 function drawSnake() {
     canvas2d.clearRect(0, 0, canvas.width, canvas.height);
-    canvas2d.fillStyle = "green"; 
     for (var i = 0; i < snakeSegments.length; i++) {
+        
+        var gradient = canvas2d.createLinearGradient(
+            snakeSegments[i].x,
+            snakeSegments[i].y,
+            snakeSegments[i].x + 10,
+            snakeSegments[i].y + 10
+        );
+        gradient.addColorStop(0, "#4caf50"); 
+        gradient.addColorStop(1, "#2e7d32"); 
+
+        canvas2d.fillStyle = gradient;
         canvas2d.fillRect(snakeSegments[i].x, snakeSegments[i].y, 10, 10);
-      }
-  }  
+
+        if (i === 0) {
+            canvas2d.fillStyle = "black";
+            canvas2d.fillRect(snakeSegments[i].x + 2, snakeSegments[i].y + 2, 2, 2); 
+            canvas2d.fillRect(snakeSegments[i].x + 6, snakeSegments[i].y + 2, 2, 2); 
+        }
+    }
+}
+
 
 function gameOver() {
     setTimeout(function() {
@@ -96,6 +132,7 @@ function gameOver() {
     gameEnded = true
   }
   function gameLoop() {
+    drawBackground();
     moveSnake();
     drawSnake();
     spawnDots();
@@ -104,4 +141,5 @@ function gameOver() {
       setTimeout(gameLoop, 100);
     }
   }
-setInterval(gameLoop, 100); 
+
+ gameLoop();
