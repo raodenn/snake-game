d60bdbca0bff5b5314343672b380b81d5a0ad3c1
raodenn <dtaye144@gmail.com>
2024-12-20 16:09:36 +0300

    added food and collision functionalities

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ var gameEnded = false;
 canvas.width = 500;
 canvas.height = 500;
 
-var directionX = 10;
+var directionX = 3;
 var directionY = 0;
 var snakeSegments = [];
 var snakeLength = 1;
 var snakeX = 0;
 var snakeY = 0;
 
+var dots = [];
+
+function spawnDots() {
+    if(dots.length < 10) {
+      var dotX = Math.floor(Math.random() * canvas.width);
+      var dotY = Math.floor(Math.random() * canvas.height);
+      dots.push({ x: dotX, y: dotY });
+    }
+    for (var i = 0; i < dots.length; i++) {
+        canvas2d.fillStyle = "red";
+        canvas2d.fillRect(dots[i].x, dots[i].y, 10, 10);
+      }
+  }
+  
 
 document.onkeydown = function(event) {
     switch (event.code) {
@@ -43,6 +57,30 @@ function moveSnake() {
       }
   }
 
+function checkCollision() {
+    for (var i = 0; i < dots.length; i++) {
+        if (snakeX < dots[i].x + 10 && 
+            snakeX + 10 > dots[i].x && 
+            snakeY < dots[i].y + 10 && 
+            snakeY + 10 > dots[i].y) {
+              snakeLength++;
+              dots.splice(i, 1);
+          }
+    }
+    if (snakeX < -10 || 
+        snakeY < -10 || 
+        snakeX > canvas.width+10 ||
+        snakeY > canvas.height+10) {
+          gameOver();
+      }
+      for (var i = 1; i < snakeSegments.length; i++) {
+        if (snakeX === snakeSegments[i].x && snakeY === snakeSegments[i].y) {
+            gameOver();
+          }
+      }
+  }
+  
+
 function drawSnake() {
     canvas2d.clearRect(0, 0, canvas.width, canvas.height);
     canvas2d.fillStyle = "green"; 
@@ -51,8 +89,19 @@ function drawSnake() {
       }
   }  
 
-function gameLoop() {
+function gameOver() {
+    setTimeout(function() {
+      alert("Game over!");
+    }, 500); 
+    gameEnded = true
+  }
+  function gameLoop() {
     moveSnake();
     drawSnake();
-}
+    spawnDots();
+    checkCollision();
+    if(!gameEnded) {
+      setTimeout(gameLoop, 100);
+    }
+  }
 setInterval(gameLoop, 100); 
